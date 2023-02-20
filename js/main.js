d18dfccfbe538247d77f429d79946aa5d6e90fe3
alicjaada83 <alicja.adamczyk83@gmail.com>
2023-02-20 19:13:15 +0100

    odpowiedz1

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let nazwa1 = "Uwielbiam JavaScript";
+let nazwa2 = "Jestem świetną programistką";
+function zwieksz(nazwa1, nazwa2) {
+let zwieksz = nazwa1 + nazwa2;
+return zwieksz;
+}
+console.log( zwieksz (nazwa1, nazwa2) );
